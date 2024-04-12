3c8c820f6c9fbe64bfbf6cee240372c2bbd9cd3a
project774fan <chinari819y2k@gmail.com>
2024-04-12 16:42:29 +0900

    image

--- src/components/array/msglist.ts
+++ src/components/array/msglist.ts
@@ -1,21 +1,5 @@
 function msglist() {
-  const msgImage = [
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-    "/img/message/12指腸_メッセージ.png",
-  ];
+  const msgImage = ["img/message/12指腸_メッセージ.png"];
   return msgImage;
 }
 export default msglist;
