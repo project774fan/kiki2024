fea81ff207d8683f44c4f83b833d02d915e9fee9
project774fan <chinari819y2k@gmail.com>
2024-06-18 00:47:00 +0900

    スマホ対応（戻るボタン）

--- src/components/ui-elements/topBtn.tsx
+++ src/components/ui-elements/topBtn.tsx
@@ -5,12 +5,12 @@ import React from "react";
 
 const TopBtn = () => {
   return (
-    <div className=" fixed bottom-6 right-10 z-10 transition-transform duration-200 hover:scale-105">
+    <div className=" fixed bottom-6 right-6 z-10 transition-transform duration-200 hover:scale-105 sm:right-10">
       <Link href={"/top"} className=" drop-shadow-lg">
-        <p className="  absolute flex h-full w-full items-center justify-center font-bold text-white hover:text-yellow-100">
+        <p className="absolute flex h-full w-full items-center justify-center text-sm font-bold text-white hover:text-yellow-100 sm:text-base">
           戻る
         </p>
-        <Image src={puppet} alt="top" className=" h-20 w-20 " />
+        <Image src={puppet} alt="top" className=" h-16 w-16 sm:h-20 sm:w-20 " />
       </Link>
     </div>
   );
