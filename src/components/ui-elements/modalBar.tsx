32570a1e78318632841fc3a53f2e7c6be486cc40
project774fan <chinari819y2k@gmail.com>
2024-06-19 00:19:47 +0900

    タブレット対応（モーダルバー）

--- src/components/ui-elements/modalBar.tsx
+++ src/components/ui-elements/modalBar.tsx
@@ -13,16 +13,18 @@ interface Handle {
 
 export const ModalBar = ({ handoleClose, logo, title, hidden }: Handle) => {
   return (
-    <div className=" absolute  z-50 flex h-12 w-full bg-violet-300 sm:h-16 ">
+    <div className=" absolute  z-50 flex h-12 w-full bg-violet-300 md:h-14 xl:h-16 ">
       <Image src={logo} alt="ロゴ" className=" w-fit object-contain py-4 pl-4 sm:pl-6" />
-      <div className="my-auto pl-2 text-2xl font-bold text-white sm:text-4xl">{title}</div>
+      <div className="my-auto pl-2 text-2xl font-bold text-white md:text-3xl xl:text-4xl">
+        {title}
+      </div>
 
       {hidden && <Image src={question} alt="ロゴ" className=" w-fit object-contain py-4 pl-3" />}
       <button
         onClick={handoleClose}
         className="ml-auto mr-6 transition-transform duration-300 hover:scale-105"
       >
-        <XMarkIcon className=" size-8 text-white sm:size-12" />
+        <XMarkIcon className=" size-8 text-white md:size-10 xl:size-12" />
       </button>
     </div>
   );
